d0cffead83a185350162e785568944b5c8004538
perlancar (on pc-office) <perlancar@gmail.com>
2019-07-29 21:38:12 +0700

    [incompatible change] Adjust to App::CSelUtils 0.082 (--actions renamed to --node-actions, add --select-action, add dump node action, etc)

--- lib/App/orgsel.pm
+++ lib/App/orgsel.pm
@@ -8,7 +8,6 @@ use strict;
 use warnings;
 
 use App::CSelUtils;
-use Scalar::Util qw(refaddr);
 
 our %SPEC;
 
@@ -16,41 +15,39 @@ $SPEC{orgsel} = {
     v => 1.1,
     summary => 'Select Org document elements using CSel (CSS-selector-like) syntax',
     args => {
-        %App::CSelUtils::foosel_common_args,
-        %App::CSelUtils::foosel_tree_action_args,
+        %App::CSelUtils::foosel_args_common,
     },
 };
 sub orgsel {
-    my %args = @_;
+     App::CSelUtils::foosel(
+        @_,
 
-    # parse first so we can bail early on error without having to read the input
-    require Data::CSel;
-    my $expr = $args{expr};
-    Data::CSel::parse_csel($expr)
-          or return [400, "Invalid CSel expression '$expr'"];
+        code_read_tree => sub {
+            require Org::Parser;
+            my $args = shift;
 
-    require Org::Parser;
-    my $parser = Org::Parser->new;
+            my $parser = Org::Parser->new;
+            my $doc;
+            if ($args->{file} eq '-') {
+                binmode STDIN, ":utf8";
+                $doc = $parser->parse(join "", <>);
+            } else {
+                local $ENV{PERL_ORG_PARSER_CACHE} = $ENV{PERL_ORG_PARSER_CACHE} // 1;
+                $doc = $parser->parse_file($args->{file});
+            }
+        },
 
-    my $doc;
-    if ($args{file} eq '-') {
-        binmode STDIN, ":utf8";
-        $doc = $parser->parse(join "", <>);
-    } else {
-        local $ENV{PERL_ORG_PARSER_CACHE} = $ENV{PERL_ORG_PARSER_CACHE} // 1;
-        $doc = $parser->parse_file($args{file});
-    }
+        csel_opts => {class_prefixes=>["Org::Element"]},
 
-    my @matches = Data::CSel::csel(
-        {class_prefixes=>["Org::Element"]}, $expr, $doc);
+        code_transform_node_actions => sub {
+            my $args = shift;
 
-    # skip root node itself
-    @matches = grep { refaddr($_) ne refaddr($doc) } @matches
-        unless @matches <= 1;
-
-    App::CSelUtils::do_actions_on_nodes(
-        nodes   => \@matches,
-        actions => $args{actions},
+            for my $action (@{$args->{node_actions}}) {
+                if ($action eq 'dump') {
+                    $action = 'dump:as_string';
+                }
+            }
+        },
     );
 }
 
